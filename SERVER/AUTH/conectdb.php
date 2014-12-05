c3ce698a4a42ccdb10b5d4a6cc78a68ec796221f
SJ <j.simon01.c@gmail.com>
2014-12-05 17:26:02 +0100

    Add start of Database use

---
+++ SERVER/AUTH/conectdb.php
@@ -0,0 +1,36 @@
+<?php
+function connect(){
+	$hostname = "localhost";
+	$username = "root";
+	$password = null;
+	$db_name = "books_mng";
+	
+	$db = null;
+	
+	try {
+		$db = new PDO('mysql:host='.$hostname.';dbname='.$db_name, $username, $password);
+	} catch (PDOException $e) {
+		echo 'Connexion échouée : ' . $e->getMessage();
+	}
+	
+	return $db;
+}
+
+
+function getResultFromDataBase($sql_request){
+
+	$db = connect();
+	
+	$sth = $db->prepare($sql_request);
+	$sth->execute();
+	
+	$result = $sth->fetchAll(PDO::FETCH_ASSOC);
+	foreach ( $result as $row){
+		$output[] = $row;
+	}
+
+	var_dump($result);
+
+	return $output;
+}
+?>
