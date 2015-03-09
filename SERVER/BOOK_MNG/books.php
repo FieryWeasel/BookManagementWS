a2d97f86a4c8cb6255bbc6496e9ab7539b1be098
SJ <j.simon01.c@gmail.com>
2015-03-09 17:37:32 +0100

    Add new features for SERVER

--- SERVER/BOOK_MNG/books.php
+++ SERVER/BOOK_MNG/books.php
@@ -17,7 +17,7 @@ dump_var($var);
 
 $isFunctionLaunched = false;
 
-if((isset($_GET) && needRest() == true) || needRest() == false){
+if((isset($_GET) && needRest()) || !needRest(){
 	switch ($name_function){
 		case "listBooks":
 			listBooks($array);
@@ -42,6 +42,35 @@ function listBooks($array) {
 	print_r(json_encode($jsonObj));
 }
 
+/*
+function createUser($userInfos){
+	$keys = new keys();
+	var_dump($userInfos);
+	
+	if(		!isset($userInfos['first_name'])
+		||	!isset($userInfos['last_name'])
+		||	!isset($userInfos['nickname'])
+		|| 	!isset($userInfos['birth_date'])
+		||	!isset($userInfos['email']))
+	{
+		displayDebugMsg("Error - Need a 'first_name' / 'last_name' / 'nickname' / 'birth_date' / 'email' argments");
+		$arr_response[$keys->ERR_key] = $keys->ERR_Bad_Arguments;
+		echo(json_encode($arr_response));
+		return;
+	}
+	
+	$res = getResultFromDataBase("INSERT INTO user (first_name, last_name, nickname, birth_date, email) VALUES ('".$userInfos['first_name']."', '".$userInfos['last_name']."', '".$userInfos['nickname']."', '".$userInfos['birth_date']."', '".$userInfos['email']."')", 'auth');
+	
+	if($res == 0){
+		$arr_response[$keys->RES_key] = $keys->RES_Result_Yes;
+		echo(json_encode($arr_response));
+	} else if($res == -1){
+		$arr_response[$keys->RES_key] = $keys->RES_Result_No;
+		echo(json_encode($arr_response));
+	}
+}
+*/
+
 function book($array) {
 	$book_id = "";
 	if(isset($array[3]) && $array[3] == 'isbn' && isset($array[4])){
@@ -62,4 +91,113 @@ function book($array) {
 		displayDebugMsg("Error - Header of function "+$array[2]+" not correct (/book/isbn/X-XXX-XXXXX-X)");
 	}
 }
+
+function add($userInfos){
+	$keys = new keys();
+	var_dump($userInfos);
+	
+	if(		!isset($userInfos['first_name'])
+		||	!isset($userInfos['last_name'])
+		||	!isset($userInfos['nickname'])
+		|| 	!isset($userInfos['birth_date'])
+		||	!isset($userInfos['email']))
+	{
+		displayDebugMsg("Error - Need a 'first_name' / 'last_name' / 'nickname' / 'birth_date' / 'email' argments");
+		$arr_response[$keys->ERR_key] = $keys->ERR_Bad_Arguments;
+		echo(json_encode($arr_response));
+		return;
+	}
+	
+	$res = getResultFromDataBase("INSERT INTO user (first_name, last_name, nickname, birth_date, email) VALUES ('".$userInfos['first_name']."', '".$userInfos['last_name']."', '".$userInfos['nickname']."', '".$userInfos['birth_date']."', '".$userInfos['email']."')", 'auth');
+	
+	if($res == 0){
+		$arr_response[$keys->RES_key] = $keys->RES_Result_Yes;
+		echo(json_encode($arr_response));
+	} else if($res == -1){
+		$arr_response[$keys->RES_key] = $keys->RES_Result_No;
+		echo(json_encode($arr_response));
+	}
+}
+
+function delete($userInfos){
+	$keys = new keys();
+
+	if(!isset($userInfos['id']))
+	{
+		displayDebugMsg("Error - Need a 'id' argment");
+		$arr_response[$keys->ERR_key] = $keys->ERR_Bad_Arguments;
+		echo(json_encode($arr_response));
+		return;
+	}
+	
+	$jsonObj = getResultFromDataBase('SELECT * FROM user WHERE id = '.$userInfos['id'], 'auth');
+	
+	if($jsonObj == ""){
+		$arr_response[$keys->ERR_key] = $keys->ERR_No_Result_Found;
+		echo(json_encode($arr_response));
+		return;
+	}
+
+	$res = getResultFromDataBase("DELETE FROM user WHERE id = ".$userInfos['id']."", 'auth');
+
+	if($res == 0){
+		$arr_response[$keys->RES_key] = $keys->RES_Result_Yes;
+		echo(json_encode($arr_response));
+	} else if($res == -1){
+		$arr_response[$keys->RES_key] = $keys->RES_Result_No;
+		echo(json_encode($arr_response));
+	}
+}
+
+function modify($userInfos){
+	$keys = new keys();
+	var_dump($userInfos);
+
+	if(!isset($userInfos['id']))
+	{
+		displayDebugMsg("Error - Need a 'id' argment");
+		$arr_response[$keys->ERR_key] = $keys->ERR_Bad_Arguments;
+		echo(json_encode($arr_response));
+		return;
+	}
+
+	$jsonObj = getResultFromDataBase('SELECT * FROM user WHERE id = '.$userInfos['id'], 'auth');
+
+	if($jsonObj == ""){
+		$arr_response[$keys->ERR_key] = $keys->ERR_No_Result_Found;
+		echo(json_encode($arr_response));
+		return;
+	}
+	
+	$strChamp = "";
+	$strValue = "";
+	$fisrt = true;
+	foreach ($userInfos as $key => $value){
+		if(		strcmp($key, 'first_name') == 0
+			||	strcmp($key, 'first_name') == 0
+			||	strcmp($key, 'first_name') == 0
+			|| 	strcmp($key, 'first_name') == 0
+			||	strcmp($key, 'first_name') == 0)
+		{
+			if($fisrt == true){
+				$strChamp .= $key.", ";
+				$strValue .= "'".$value."'";
+			} else {
+				$strChamp .= ", ".$key;
+				$strValue .= ", '".$value."'";
+			}
+		}
+	}
+
+	$res = getResultFromDataBase("", 'auth');
+
+	if($res == 0){
+		$arr_response[$keys->RES_key] = $keys->RES_Result_Yes;
+		echo(json_encode($arr_response));
+	} else if($res == -1){
+		$arr_response[$keys->RES_key] = $keys->RES_Result_No;
+		echo(json_encode($arr_response));
+	}
+}
+
 ?>
--- SERVER/keys.php
+++ SERVER/keys.php
@@ -1,10 +1,11 @@
 <?php
 class keys{
-	public $ERR_key = 'err';
+	public $ERR_key = 'error';
 	public $ERR_Bad_Arguments = '-1';
 	public $ERR_No_Result_Found = '-2';
+	public $ERR_Add_Key_Already_Exist = '-3';
 	
-	public $RES_key = 'res';
+	public $RES_key = 'response';
 	public $RES_Result_No = 'NO';
 	public $RES_Result_Yes = 'YES';
 }
